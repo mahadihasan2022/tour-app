3da43198d57f2367cfd95a4d0d45b91fb23103da
mahadihasan2022 <mahadihasanmim72@gmail.com>
2023-03-31 12:48:43 +0600

    banner section

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from '../../Share/Banner/Banner';
+import CurrentPackage from '../../Share/CurrentPackage/CurrentPackage';
+
+const Home = () => {
+    return (
+        <div>
+            <Banner/>
+            <CurrentPackage/>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Share/CurrentPackage/CurrentPackage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const CurrentPackage = () => {
+    return (
+        <div>
+            <h1 style={{textAlign:'center',fontSize:'35px', fontWeight:'bold',fontStyle:'italic'}}>Current Hot Package</h1>
+
+
+
+
+        </div>
+    );
+};
+
+export default CurrentPackage;
