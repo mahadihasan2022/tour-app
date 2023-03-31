b6a06cdfe5f79be5f4205628a55980af75514d0c
mahadihasan2022 <mahadihasanmim72@gmail.com>
2023-03-31 13:03:27 +0600

    upload

--- src/Share/Banner/Banner.jsx
+++ src/Share/Banner/Banner.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
-// import alaska from '../../Source/Video/alaska.mp4';
+import alaska from '../../Source/Video/alaska.mp4';
 import './Banner.css';
 
 const Banner = () => {
     return (
         <div className='banner'>
-            {/* <video className='bg-video' src={alaska} autoPlay loop muted></video>
-            <div className='bg-overlay'></div> */}
-            <h1>mmmmmm</h1>
-            <h1>mmmmmm</h1>
-
+            <video className='bg-video' src={alaska} autoPlay loop muted></video>
+            <div className='bg-overlay'></div>
         </div>
     );
 };
