ce3011eb5c1f0b937443a52464013e5f0b9c0c5f
mahadihasan2022 <mahadihasanmim72@gmail.com>
2023-05-25 01:09:26 +0600

    create context & firebase config

---
+++ src/firebase/firebase.config.jsx
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBo546yyou6qWzFqWLsnm1_Nd79GLGFTe8",
+  authDomain: "traveler-s-destination-bd.firebaseapp.com",
+  projectId: "traveler-s-destination-bd",
+  storageBucket: "traveler-s-destination-bd.appspot.com",
+  messagingSenderId: "854300716356",
+  appId: "1:854300716356:web:e500d38c568dd5e2343b11"
+};
